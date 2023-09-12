913d5ece8af58d91a0e3dcfdf6d0e5c68b0bfd22
AndresG <andres.giuffre@educacionit.com>
2023-09-11 21:59:19 -0300

    Clase 3

--- Clases/clase2.py
+++ Clases/clase2.py
@@ -28,7 +28,7 @@ lambda x : x ** 2
 
 
 # EXCEPCIONES
-
+"""
 try:
     int('1')
 except (ValueError, Exception):
@@ -41,4 +41,86 @@ else:
     print("Todo salió bien")
 finally:
     print("Se terminó el bloque")
+"""
 
+# OPERACIONES CON STRINGS
+
+texto = "Ejemplo, de caracteres, especiales" # \t tabulado, \n salto de línea
+#print(texto[2:]) # Slicing
+#print(texto * 2)
+#print(texto.startswith("Ejem"))
+#print(texto.endswith("especiales"))
+#print(texto.strip()) # Quita espacios iniciales y finales
+#texto = texto.replace("especiales", "espaciales") # Reemplaza una cadena de texto por otra
+#texto = texto.split(',') # Crea una lista con elementos delimitando por comas
+#print(texto.find("caracteres")) # Busca y devuelve el índice donde se encuentra la primer letra buscada
+#print(texto.upper()) # Devuelve todo el texto en mayúsculas
+#print(texto.lower()) # Devuelve todo el texto en minúsculas
+#print(texto.capitalize()) # Pone en mayúsculas la primer letra del texto
+
+# FORMACION DE CADENAS #1
+"""
+nombre = "Juan"
+edad = 20
+mensaje = "Hola, me llamo {0} y tengo {1} años"
+print(mensaje.format(nombre, edad))
+"""
+
+# FORMACION DE CADENAS #2
+"""
+nombre = "María"
+edad = 30
+mensaje = f"Hola, me llamo {nombre} y tengo {edad} años"
+print(mensaje)
+"""
+
+# ENTRADA Y SALIDA DE ARCHIVOS
+
+#with expresion as variable:
+    # Bloque de codigo
+
+# Parámetros de open() --> "w" (write/escritura) - "r" (read/lectura) - "a" (append/agregar)
+"""
+with open("archivo.txt", "w") as archivo:
+    archivo.write("Hola, aca estoy!")
+
+with open("archivo.txt", "a") as archivo:
+    archivo.write("\nSeguimos agregando contenido")
+
+with open("archivo.txt", "r") as archivo:
+    print(archivo.read())
+"""
+
+"""
+archivo = open("archivo.txt", "r")
+print(archivo.read())
+print(archivo.readlines())
+archivo.close()
+"""
+
+"""
+texto = ["Primera linea\n", "Segunda linea\n", "Tercera linea\n"]
+archivo = open("archivo2.txt", "w")
+archivo.writelines(texto)
+archivo.close()
+archivo = open("archivo2.txt", "r")
+print(archivo.read())
+archivo.close()
+"""
+
+"""
+texto = ["Primera linea\n", "Segunda linea\n", "Tercera linea\n"]
+archivo = open("archivo2.txt", "r+")
+archivo.writelines(texto)
+archivo.seek(0)
+print(archivo.read())
+archivo.close()
+"""
+
+"""
+with open("archivo2.txt", "r+") as archivo:
+    texto = ["Primera linea\n", "Segunda linea\n", "Tercera linea\n"]
+    archivo.writelines(texto)
+    archivo.seek(0)
+    print(archivo.read())
+"""
--- Clases/ejercicios.py
+++ Clases/ejercicios.py
@@ -1,30 +1,51 @@
 """
-lista = [0, 1]
+lista = ["juan salvo", "henry courtney", "elizabeth bennet", "marge simpson"]
 
-def fibo(num):
-    if num <= 2:
-        print("El numero debe ser mayor a 2")
-    else:
-        for i in range(2, num):
-            lista.append(lista[i-2] + lista[i-1])
-    return lista
-
-
-print(fibo(2))
+i=0
+for cad in lista:
+    lista[i]= lista[i].title()
+    i+=1
+print(lista)
 """
 
-def fibo(n):
-    if n <= 2:
-        return "El número debe ser mayor que 2."
-        
-    fibonacci = [0, 1]
-    
-    while len(fibonacci) < n:
-        next = fibonacci[-1] + fibonacci[-2]
-        fibonacci.append(next)
-    
-    return fibonacci
+"""
+def capitalizar(lista):
+    resultado= []
 
-prueba = 2
-resultado = fibo(prueba)
+    for nombre in lista:
+        
+        nombre_apellido = nombre.split()
+        if len(nombre_apellido) == 2:
+            nombre, apellido = nombre_apellido
+            nombre = nombre.capitalize()
+            apellido = apellido.capitalize()
+            nombre_capitalizado = f"{nombre} {apellido}"
+            resultado.append(nombre_capitalizado)
+
+    return resultado
+
+lista = ["juan salvo", "henry courtney", "elizabeth bennet", "marge simpson"]
+
+resultado = capitalizar(lista)
+
 print(resultado)
+"""
+
+"""
+lista = ["juan salvo", "henry courtney", "elizabeth bennet", "marge simpson"]
+
+def mayusculas(lista):
+    contador = 0
+    nueva = []
+
+    while contador < len(lista):
+        separador = lista[contador].split()
+        auxiliar = separador[0].capitalize() + " " + separador[1].capitalize()
+        nueva.append(auxiliar)
+        contador += 1
+
+    print(nueva)
+
+
+mayusculas(lista)
+"""
